15e71b7bc0fd13a0f98f697663d8ce01382f35a3
Jeremy Clark <jtclark1825@eagle.fgcu.edu>
2025-03-18 00:35:36 -0400

    Updated dependencies for UI elements and MVC component interactions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -74,6 +73,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation(libs.androidx.room.runtime.android)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
